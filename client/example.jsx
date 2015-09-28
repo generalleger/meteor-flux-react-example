af983134f217c8866aaa7faa6b5e2da5adb84767
Christopher Moschella <cmoschella@ChrisMBP.local>
2015-09-28 12:51:29 -0400

    simplified example

--- client/example.jsx
+++ client/example.jsx
@@ -6,23 +6,25 @@ Example = React.createClass({
   mixins: [ReactMeteorData],
 
   // getMeteorData() is listening to two reactive data sources.
-  // - countSessionVar: A Meteor session variable and is listening
-  // - Store.addOneReactiveVar(): Aside from the name, it is hard to
+  // - Session.get("addOneSessionVar") -  A Meteor session variable and is listening
+  // - Store.addOneReactiveVar() - Aside from the name, it is hard to
   //   tell that this is a Meteor ReactiveVar.  The ReactiveVar is being
   //   returned from a method called addOneReactiveVar in the app's flux store.
-
   getMeteorData(){
-    // everything returned from getMeteorData() is accessed in React as
-    // this.data.something I am returning
+    // everything returned from getMeteorData() is accessed in React as this.data.someData
     return {
       countSessionVar: Session.get("addOneSessionVar"),
       countReactiveVar: Store.addOneReactiveVar()
     };
   },
   handleAddOneSession(){
+    // Send an object to the dispatcher that indicates the action type and the
+    // data that we want to do something with
     Dispatcher.dispatch( { actionType: "ADD_ONE_SESSION_VAR", countSessionVar: this.data.countSessionVar } );
   },
   handleAddOneReactiveVar(){
+    // Send an object to the dispatcher that indicates the action type and the
+    // data that we want to do something with
     Dispatcher.dispatch( { actionType: "ADD_ONE_REACTIVE_VAR", countReactiveVar: this.data.countReactiveVar } );
   },
   render() {
--- client/flux.js
+++ client/flux.js
@@ -1,16 +1,17 @@
 
+// Create the Session Variable and set the initial value
 Session.setDefault("addOneSessionVar", 1);
 
-// Create the reactiveVar and set the initial value
+// Create the Reactive Variable and set the initial value
 var currentCountReactiveVar =  new ReactiveVar(1);
 
-// Function to mutate the value provided from the button click
-var addOneReactiveVar = (currentVal) => {
-  currentCountReactiveVar.set((currentVal) + 1);
-};
-
+////////////////////////////////////////////////////
 // The dispatcher is receiving the action when the button
 // click in the UI occurs.
+//
+// In this example the data mutations are simple and are handled
+// within the dispatcher.  More complex mutations can be
+// abstracted away to keep the dispatcher organized
 Dispatcher.register(function(payload){
   switch( payload.actionType ){
     case "ADD_ONE_SESSION_VAR":
@@ -19,15 +20,14 @@ Dispatcher.register(function(payload){
       break;
 
     case "ADD_ONE_REACTIVE_VAR":
-      // Call the function to mutate the data
-      addOneReactiveVar(payload.countReactiveVar);
+      // Add one to our reactive variable
+      currentCountReactiveVar.set((payload.countReactiveVar) + 1);
       break;
     }
 });
 
 Store = {
   addOneSessionVar: function(currentCount){
-    //console.log(currentCount);
     Session.set("addOneSessionVar", currentCount + 1);
     return;
   },
